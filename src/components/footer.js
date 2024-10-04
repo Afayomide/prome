9ad6744e988b393cd3ba9fe7e51ac5ba76db556e
afayomide <daraseyi086@gmail.com>
2024-10-04 13:54:10 +0100

    new commit

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return(
+        <footer>
+<h3>Prome Collection</h3>
+<p>© 2024 Prome Collection. All rights reserved.
+</p>
+<p>Reproduction, in whole or in part, is prohibited.</p>
+        </footer>
+    )
+}
+
+export default Footer
